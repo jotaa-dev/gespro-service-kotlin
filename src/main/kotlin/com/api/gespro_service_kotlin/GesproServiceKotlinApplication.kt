b5e213290c3160dfa4b10e0050e8a327502b92a8
Jonathan Ismael Balbuena <jonathan.jib@hotmail.com>
2024-06-13 17:48:03 -0300

    Initial Configuration

---
+++ src/main/kotlin/com/api/gespro_service_kotlin/GesproServiceKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.api.gespro_service_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GesproServiceKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<GesproServiceKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/gespro_service_kotlin/GesproServiceKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.gespro_service_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GesproServiceKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
